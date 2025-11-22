4d2ae69c9e0d8ea0abaef532a6c302dc8ac7f786
mahtuag-coder <gautham.malapati@gmail.com>
2025-11-22 13:22:39 -0600

    Basic setup

---
+++ src/test/java/ws_orders_wimf/WsOrdersWimfApplicationTests.java
@@ -0,0 +1,13 @@
+package ws_orders_wimf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsOrdersWimfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
